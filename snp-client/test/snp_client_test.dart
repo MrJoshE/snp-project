b0773604f0ebf7d86daafdb9dc07d5f3813ea031
MrJoshE <josheverett02@gmail.com>
2022-04-20 08:15:21 +0100

    Added mocking package to client pubspec

--- snp-client/test/snp_client_test.dart
+++ snp-client/test/snp_client_test.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:snp_client/abstract/snp_client.dart';
+import 'package:snp_client/abstract/snp_client_options.dart';
+import 'package:snp_client/snp_client_impl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late SnpClient _client;
+  late SnpClientOptions _options;
+
+  setUp(() {
+    _options = SnpClientOptions();
+    _client = SnpClientImpl(_options);
+  });
+
+  group('[SnpClient]', () {
+    test('can be created', () async {
+      expect(_client, isNotNull);
+    });
+  });
+
+  tearDown(() {});
+}
